896671ff6373e3f8313fe9ef77df09f43784e206
abdubey1024 <112496737+abdubey1024@users.noreply.github.com>
2023-03-23 15:24:34 +0530

    Update Main.java

--- Patterns/src/Main.java
+++ Patterns/src/Main.java
@@ -62,6 +62,13 @@ public class Main{
 //        123
 //        1234
 //        12345
+//         int n = 5;
+//         for(int i=1; i<=n;i++){
+//             for(int j=1; j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
 
 
     }
