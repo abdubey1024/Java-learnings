a45af9f633caad99a3e92ab363368ac742b5b1e1
abdubey1024 <112496737+abdubey1024@users.noreply.github.com>
2023-03-24 11:36:02 +0530

    Add files via upload

---
+++ Array Lecture 10/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.*;
+//print marks of 3 subjects using array
+public class Main {
+    public static void main(String[] args) {
+//        int [] marks = new int [3];
+//        marks [0] = 89;
+//        marks[1] = 90;
+//        marks[2] = 98;
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+
+        //another way to print :
+
+//        for(int i=0; i<3; i++){
+//            System.out.println(marks[i]);
+//        }
+
+
+        //size input and default value initialization in arrays:
+//        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt();
+//        int[] x =new int[size];
+//
+//        for(int i=0; i<=size; i++){
+//            System.out.println(x[i]);
+//        }
+        //Take an array as input from the user.Search for a given number x and print the index at which it occurs
+
+//        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt(); //input array size
+//        int n []= new int[size]; // define array
+
+        //input array values
+//        for(int i=0; i<size; i++){
+//            n[i]= sc.nextInt();
+//        }
+
+        //input the number that is to be found
+        //int x = sc.nextInt();
+        //output
+//        for (int i=0; i<n.length; i++){
+//            if (n[i] == x){
+//                System.out.println(x +" found at index "+i);
+//            }
+//        }
+
+
+
+
+    }
+}
